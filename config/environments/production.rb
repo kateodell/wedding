296e260122d737d02043bf08003b7bac4cd17bf8
Kate Odell <kate.m.odell@gmail.com>
2013-12-18 19:01:04 -0800

    before restarting w/ new design

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Wedding::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.1'
 
 
   # added as per activeadmin instructions
