f9ee929073d32ce2aee4f5541b6a2777bca62c5c
Kate Odell <kate.m.odell@gmail.com>
2014-01-11 23:03:44 -0800

    dont break things

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,12 +1,6 @@
 class Guest < ActiveRecord::Base
     has_one :group
 
-    attr_accessible :phone, 
-                    :contact_preference, 
-                    :mailing_address, :city, :state, :zip, 
-                    :group_id, 
-                    :rsvp_status, :rehearsal_status, 
-
     before_save { self.email = email.downcase }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
